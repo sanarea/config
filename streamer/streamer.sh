60f77de38776df424ddcfd2b50c09b4038d0cabc
yangchigi <yangchigi@yangchigi.com>
2019-08-08 19:04:30 +0900

    sfd

--- streamer/streamer.sh
+++ streamer/streamer.sh
@@ -3,5 +3,5 @@
 
 mjpg_streamer -i "input_uvc.so -d /dev/video0 -n -f 15 -r 1280x720" \
                 -i "input_uvc.so -d /dev/video4 -n -f 15 -r 1280x720" \
-                -o "output_http.so -w /usr/share/mjpg_streamer/www  -p 8181"
+                -o "output_http.so -w /usr/share/mjpg_streamer/www  -p 8001"
  
