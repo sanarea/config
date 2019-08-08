2ec697ae62610969bc46c2ce0aea343843329f48
yangchigi <yangchigi@yangchigi.com>
2019-08-08 19:03:10 +0900

    sdf

--- streamer/install.sh
+++ streamer/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo cp mjpeg-streamer.service /etc/systemd/system
+sudo cp streamer.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable streamer.service
 sudo systemctl start streamer.service
